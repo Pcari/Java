2a27b22462ff091b58b185dc9ea6f0f8b381a089
bensjae <bensjae@gmail.com>
2018-12-21 21:05:03 -0800

    fix statements

--- mobilePhone/src/com/company/MobilePhone.java
+++ mobilePhone/src/com/company/MobilePhone.java
@@ -25,6 +25,9 @@ public class MobilePhone {
         if (foundPosition < 0) {
             System.out.println(oldContact.getName() + ", was not found.");
             return false;
+        } else if (findContact(newContact.getName()) != -1) {
+            System.out.println("Contact already exists");
+            return false;
         }
         this.myContacts.set(foundPosition, newContact);
         System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
